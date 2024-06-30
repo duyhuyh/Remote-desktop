54004dc9cde2c54d38d96734843d0f43510918d1
Duy <22520330@gm.uit.edu.vn>
2024-07-01 03:43:25 +0700

    init project

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace app
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBoxClients = new System.Windows.Forms.ListBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnListen = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(916, 571);
+            this.pictureBoxDisplay.TabIndex = 0;
+            this.pictureBoxDisplay.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.btnListen);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.btnSend);
+            this.groupBox1.Controls.Add(this.listBoxClients);
+            this.groupBox1.Location = new System.Drawing.Point(681, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 400);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // listBoxClients
+            // 
+            this.listBoxClients.FormattingEnabled = true;
+            this.listBoxClients.ItemHeight = 16;
+            this.listBoxClients.Location = new System.Drawing.Point(6, 181);
+            this.listBoxClients.Name = "listBoxClients";
+            this.listBoxClients.Size = new System.Drawing.Size(223, 116);
+            this.listBoxClients.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(154, 358);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 36);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 312);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 40);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(17, 51);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(75, 23);
+            this.btnListen.TabIndex = 3;
+            this.btnListen.Text = "Listen";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Click += new System.EventHandler(this.btnListen_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(137, 51);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 571);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.KeyPreview = true;
+            this.Name = "App";
+            this.Text = "App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.ListBox listBoxClients;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app
+{
+    public partial class App : Form
+    {
+        /*for server*/
+
+        private TcpListener tcpListener;
+        private Thread listenThread;
+        private List<TcpClient> clients = new List<TcpClient>();
+
+        /*--------------------------------------------------------*/
+
+        /*for client*/
+
+        private TcpClient client;
+        private NetworkStream serverStream;
+        private Thread captureThread;
+        private Thread commandThread;
+
+        /*--------------------------------------------------------*/
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /*-----------------------------SERVER---------------------------------------------------------*/
+
+        private void btnListen_Click(object sender, EventArgs e)
+        {
+            tcpListener = new TcpListener(IPAddress.Any, 5000);
+            listenThread = new Thread(new ThreadStart(ListenForClients));
+            listenThread.Start();
+            listBoxClients.Items.Add("Server started...");
+        }
+
+        private void ListenForClients()
+        {
+            tcpListener.Start();
+
+            while (true)
+            {
+                TcpClient client = tcpListener.AcceptTcpClient();
+                clients.Add(client);
+                listBoxClients.Invoke(new Action(() => listBoxClients.Items.Add("Client connected...")));
+
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+                clientThread.Start(client);
+            }
+        }
+
+        private void HandleClientComm(object client_obj)
+        {
+            TcpClient tcpClient = (TcpClient)client_obj;
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            while (true)
+            {
+                byte[] sizeBytes = new byte[4];
+                clientStream.Read(sizeBytes, 0, sizeBytes.Length);
+                int size = BitConverter.ToInt32(sizeBytes, 0);
+
+                byte[] data = new byte[size];
+                int bytesRead = 0;
+                while (bytesRead < size)
+                {
+                    bytesRead += clientStream.Read(data, bytesRead, size - bytesRead);
+                }
+
+                using (MemoryStream ms = new MemoryStream(data))
+                {
+                    pictureBoxDisplay.Image?.Dispose(); // Dispose previous image to release resources
+                    pictureBoxDisplay.Image = new Bitmap(ms);
+                }
+            }
+
+        }
+
+        private void SendCommandToClient(string command)
+        {
+            foreach (TcpClient client in clients)
+            {
+                NetworkStream clientStream = client.GetStream();
+                byte[] commandBytes = Encoding.ASCII.GetBytes(command);
+                clientStream.Write(commandBytes, 0, commandBytes.Length);
+            }
+        }
+
+        private bool ShiftPress = false;
+        private string t = "";
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ControlKey) { t += "^"; }
+            else if (e.KeyCode == Keys.Tab) { t += "{TAB}"; }
+            else if (e.KeyValue == 18) { t += "%"; }
+            else if (e.KeyCode == Keys.ShiftKey) { t += "+"; ShiftPress = true; }
+            /*else if (e.KeyCode.ToString() == "F0") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "F1") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "F2") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "F3") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "F4") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "F5") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "F6") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "F7") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "F8") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "F9") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "F10") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "F11") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "F12") { t += "{" + e.KeyCode + "}"; }*/
+            else if (e.Shift && e.KeyCode.ToString().StartsWith("F") && e.KeyCode.ToString().Length != 1) { t += ("{" + e.KeyCode + "}"); }
+            else if (e.Shift && e.KeyCode.ToString() == "D0") { t += "{)}"; }
+            else if (e.Shift && e.KeyCode.ToString() == "D1") { t += "{!}"; }
+            else if (e.Shift && e.KeyCode.ToString() == "D2") { t += "{@}"; }
+            else if (e.Shift && e.KeyCode.ToString() == "D3") { t += "{#}"; }
+            else if (e.Shift && e.KeyCode.ToString() == "D4") { t += "{$}"; }
+            else if (e.Shift && e.KeyCode.ToString() == "D5") { t += "{%}"; }
+            else if (e.Shift && e.KeyCode.ToString() == "D6") { t += "{^}"; }
+            else if (e.Shift && e.KeyCode.ToString() == "D7") { t += "{&}"; }
+            else if (e.Shift && e.KeyCode.ToString() == "D8") { t += "{*}"; }
+            else if (e.Shift && e.KeyCode.ToString() == "D9") { t += "{(}"; }
+            else if (e.KeyCode.ToString().StartsWith("D") && e.KeyCode.ToString().Length != 1) { t += (e.KeyCode.ToString().Substring(1)); }
+            /*else if (e.KeyCode.ToString() == "NumPad0") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "NumPad1") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "NumPad2") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "NumPad3") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "NumPad4") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "NumPad5") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "NumPad6") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "NumPad7") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "NumPad8") { t += "{" + e.KeyCode + "}"; }
+            else if (e.KeyCode.ToString() == "NumPad9") { t += "{" + e.KeyCode + "}"; }*/
+            else if (e.KeyCode.ToString().StartsWith("NumPad")) { t += ("{" + e.KeyCode + "}"); }
+            else if (e.KeyCode.ToString() == "Up") { t += "{UP}"; }
+            else if (e.KeyCode.ToString() == "Down") { t += "{DOWN}"; }
+            else if (e.KeyCode.ToString() == "Left") { t += "{LEFT}"; }
+            else if (e.KeyCode.ToString() == "Right") { t += "{RIGHT}"; }
+            else if (e.KeyCode.ToString() == "Return") { t += "{ENTER}"; }
+            else if (e.KeyCode.ToString() == "Capital") { t += "{CAPLOCK}"; }
+            else if (e.KeyCode.ToString() == "Escape") { t += "{ESC}"; }
+            else if (e.KeyCode.ToString() == "Back") { t += "{BACKSPACE}"; }
+            else if (e.KeyCode.ToString() == "Space") { t += " "; }
+            else if (e.KeyCode.ToString() == "NumLock") { t += "{NUMLOCK}"; }
+            else if (e.KeyCode.ToString() == "End") { t += "{END}"; }
+            else if (e.KeyCode.ToString() == "Home") { t += "{HOME}"; }
+            else if (e.KeyCode.ToString() == "Next") { t += "{PGDN}"; }
+            else if (e.KeyCode.ToString() == "PageUp") { t += "{PGIP}"; }
+            else if (e.KeyCode.ToString() == "Space") { t += "{SPACE}"; }
+            else if (e.KeyCode.ToString() == "Delete") { t += "{DELETE}"; }
+            else if (e.KeyCode.ToString() == "Pause") { t += "{BREAK}"; }
+            else if (e.KeyCode.ToString() == "Insert") { t += "{INSERT}"; }
+            else if (e.KeyCode.ToString() == "Break") { t += "{SPACE}"; }
+            else if (e.Shift && e.KeyValue == 189) { t += "{_}"; }
+            else if (e.Shift && e.KeyValue == 187) { t += "{+}"; }
+            else if (e.Shift && e.KeyValue == 219) { t += "{"; }
+            else if (e.Shift && e.KeyValue == 221) { t += "}"; }
+            else if (e.Shift && e.KeyValue == 220) { t += "|"; }
+            else if (e.Shift && e.KeyValue == 186) { t += "{:}"; }
+            else if (e.Shift && e.KeyValue == 222) { t += "\""; }
+            else if (e.Shift && e.KeyValue == 188) { t += "<"; }
+            else if (e.Shift && e.KeyValue == 190) { t += ">"; }
+            else if (e.Shift && e.KeyValue == 191) { t += "?"; }
+            else if (e.Shift && e.KeyValue == 192) { t += "~"; }
+            else if (e.KeyValue == 192) { t += "`"; }
+            else if (e.KeyValue == 189) { t += "{-}"; }
+            else if (e.KeyValue == 187) { t += "{=}"; }
+            else if (e.KeyValue == 219) { t += "["; }
+            else if (e.KeyValue == 221) { t += "]"; }
+            else if (e.KeyValue == 220) { t += "\\"; }
+            else if (e.KeyValue == 186) { t += "{;}"; }
+            else if (e.KeyValue == 222) { t += "'"; }
+            else if (e.KeyValue == 188) { t += ","; }
+            else if (e.KeyValue == 190) { t += "."; }
+            else if (e.KeyValue == 191 || e.KeyValue == 111) { t += "/"; }
+            else if (e.KeyValue == 106) { t += "*"; }
+            else if (e.KeyValue == 109) { t += "-"; }
+            else if (e.KeyValue == 107) { t += "{+}"; }
+            else if (e.Shift && e.KeyValue >= 65 && e.KeyValue <= 90) { t += "+" + e.KeyCode.ToString().ToLower(); }
+            else if (e.KeyValue >= 65 && e.KeyValue <= 90) { t += e.KeyCode.ToString().ToLower(); }
+            else { t = t + e.KeyCode + " - " + e.KeyValue; }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ShiftKey) { ShiftPress = false; }
+            if (t.Length != 0)
+            {
+/*                Console.Write(t.Length);
+                Console.WriteLine(" - " + t.Replace("++", "+"));*/
+                SendCommandToClient("KEYBOARD:" + t.Replace("++", "+"));
+            }
+            t = "";
+        }
+
+
+        /*---------------------------------------------------------------------------------------------*/
+
+        /*-----------------------------CLIENT----------------------------------------------------------*/
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            client = new TcpClient("127.0.0.1", 5000);
+            serverStream = client.GetStream();
+            captureThread = new Thread(new ThreadStart(CaptureScreen));
+            captureThread.Start();
+
+            commandThread = new Thread(new ThreadStart(ListenForCommands));
+            commandThread.Start();
+        }
+
+        private void CaptureScreen()
+        {
+            while (true)
+            {
+                Rectangle bound = Screen.PrimaryScreen.Bounds;
+                Bitmap screenshot = new Bitmap(1920, 1080, PixelFormat.Format32bppArgb);
+                Graphics graphics = Graphics.FromImage(screenshot);
+                graphics.CopyFromScreen(0, 0, 0, 0, screenshot.Size, CopyPixelOperation.SourceCopy);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    ImageCodecInfo jpgEncoder = ImageCodecInfo.GetImageEncoders().FirstOrDefault(c => c.FormatID == ImageFormat.Jpeg.Guid);
+                    EncoderParameters encoderParams = new EncoderParameters(1);
+                    encoderParams.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 80L);
+                    screenshot.Save(ms, jpgEncoder, encoderParams);
+
+                    byte[] data = ms.ToArray();
+                    byte[] sizebyte = BitConverter.GetBytes(data.Length);
+
+                    serverStream.Write(sizebyte, 0, sizebyte.Length);
+                    serverStream.Write(data, 0, data.Length);
+                }
+
+                Thread.Sleep(100);
+            }
+        }
+
+        private void ListenForCommands()
+        {
+            while (true)
+            {
+                byte[] commandBuffer = new byte[4096];
+                int bytesRead = serverStream.Read(commandBuffer, 0, commandBuffer.Length);
+                if (bytesRead > 0)
+                {
+                    string command = Encoding.ASCII.GetString(commandBuffer, 0, bytesRead);
+                    ExecuteCommand(command);
+                }
+            }
+        }
+
+        private void ExecuteCommand(string command)
+        {
+            string[] commandParts = command.Split(':');
+            switch (commandParts[0])
+            {
+                case "KEYBOARD":
+                    /*SendKeys.SendWait(commandParts[1]);*/
+                    Console.WriteLine(commandParts[1]);
+                    break;
+                case "MOVE_MOUSE":
+                    int x = int.Parse(commandParts[1]);
+                    int y = int.Parse(commandParts[2]);
+                    SetCursorPos(x, y);
+                    break;
+                case "CLICK_MOUSE":
+                    if (commandParts[1] == "LEFT")
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    }
+                    break;
+                case "SEND_KEYSTROKE":
+                    char key = commandParts[1][0];
+                    SendKeys.SendWait(key.ToString());
+                    break;
+            }
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+
+    }
+}
+
